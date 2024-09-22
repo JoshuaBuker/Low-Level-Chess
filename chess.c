d43a40cab7bd1f53a55557865788959c7876c3f7
Joshua Buker <joshbuker04@gmail.com>
2024-09-22 00:25:44 -0700

    first commit

---
+++ chess.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "func.h"
+
+int main(void) {
+  printf("%d", add(10, 20));
+
+  return 1;
+}
---
+++ func.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+int add(const int x, const int y);
+
+#endif
